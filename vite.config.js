4b5e8e834feb6d8a77691e45e2ec169872d874d1
Camille <c.hughes64@gmail.com>
2022-06-13 23:16:25 -0400

    vite config bug test #1

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-import { fileURLToPath, URL } from 'url'
+// import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+});
