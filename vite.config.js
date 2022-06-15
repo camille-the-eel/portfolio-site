945e8541fa76b5ca6290063e92709d3d7d764466
Camille <c.hughes64@gmail.com>
2022-06-15 09:58:56 -0400

    testing jsconfig resolution

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,12 @@ export default defineConfig({
   },
 });
 
+//GET https://camille-the-eel.github.io/src/main.js net::ERR_ABORTED 404
+//GET https://camille-the-eel.github.io/favicons/site.webmanifest 404
+//Manifest: Line: 1, column: 1, Syntax error.
+
+//Uncaught TypeError: Failed to resolve module specifier "vue". Relative references must start with either "/", "./", or "../".
+
 // import path from "path";
 // import { defineConfig } from "vite";
 // import vue from "@vitejs/plugin-vue";
